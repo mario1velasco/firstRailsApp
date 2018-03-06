3e97bf40bb448d9106d560bc2f588844efda0b1e
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-06 10:13:23 +0100

    Feat: - Create view with form - Controller get the form values through post action - Create Model and migration - Migrate to the schema - Crontroller Create an article in the Model (C of CRUD)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
   def new
   end
+  def create
+    # art= params[:article]
+    # puts "El titulo es = #{art["title"]}"
+    # render plain: params[:article].inspect
+
+    # @article = Article.new(params[:article]) 
+
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
